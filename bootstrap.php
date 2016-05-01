21b52c3f0560f78a8cfeb010e0429c76b4f0560d
Rezouce <contact@rezouce.net>
2016-05-01 13:57:28 +0200

    Change default bootstrap file for PHPUnit and add path constant for cache and tests directories.

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+if (!defined('CACHE_PATH')) {
+    define('CACHE_PATH', __DIR__ . '/cache');
+}
+
+if (!defined('TESTS_PATH')) {
+    define('TESTS_PATH', __DIR__ . '/tests');
+}
--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -17,34 +17,28 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
 
     public function testItGenerateLanguageFiles()
     {
-        $directoryCache = __DIR__ . '/../cache/portal';
-        $directoryOutput = __DIR__ . '/output';
-
         $this->languageBatchBo->generateLanguageFiles();
 
         $this->assertEquals(
-            file_get_contents($directoryOutput . '/en.php'),
-            file_get_contents($directoryCache . '/en.php'),
+            file_get_contents(CACHE_PATH . '/portal/en.php'),
+            file_get_contents(TESTS_PATH . '/output/en.php'),
             'EN language file'
         );
 
         $this->assertEquals(
-            file_get_contents($directoryOutput . '/hu.php'),
-            file_get_contents($directoryCache . '/hu.php'),
+            file_get_contents(CACHE_PATH . '/portal/hu.php'),
+            file_get_contents(TESTS_PATH . '/output/hu.php'),
             'HU language file'
         );
     }
 
     public function testItGenerateAppletLanguageXmlFiles()
     {
-        $directoryCache = __DIR__ . '/../cache/flash';
-        $directoryOutput = __DIR__ . '/output';
-
         $this->languageBatchBo->generateAppletLanguageXmlFiles();
 
         $this->assertEquals(
-            file_get_contents($directoryOutput . '/lang_en.xml'),
-            file_get_contents($directoryCache . '/lang_en.xml')
+            file_get_contents(CACHE_PATH . '/flash/lang_en.xml'),
+            file_get_contents(TESTS_PATH . '/output/lang_en.xml')
         );
     }
 }
